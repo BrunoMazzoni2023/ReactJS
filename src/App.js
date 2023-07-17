055ebbdcc0c32a7bf80edf3e2c8d956c99cb3e8e
Bruno Ferraz Mazzoni <bruno.bmxdabiciceta@gmaol.com>
2023-07-17 18:13:56 -0300

    Praticando-Tarefa-ReactJS

--- src/App.js
+++ src/App.js
@@ -4,67 +4,43 @@ import Nome from "./components/Nome";
 
 
 function App() {
-  const [nome, setNome] = useState('');
-  const [email, setEmail] = useState('');
-  const [idade, setIdade] = useState(0);
-
-  const [user, setUser] = useState({});
+  const [input, setInput] = useState('');
+  const [tarefas, setTarefas] = useState([
+    'Pagar a conta de luz',
+    'Estudar React JS'
+  ]);
 
   function handleRegister(e){
     e.preventDefault();
-    
-  alert("Registrado com sucesso");
-  setUser({
-    
-    nome: nome,
-    idade: idade,
-    email: email,
-  })
-  }
 
+    setTarefas([...tarefas, input])
+    setInput('');
+   }
 
   return (
     <div>
       <h1>Cadastrando usuario</h1>
 
       <form onSubmit={handleRegister}>
-        <label>Nome: </label><br/>
+        <label>Nome da Tarefa </label><br/>
         <input 
-        placeholder="Digite seu Nome:" 
-        value={nome} 
-        onChange={ (e) => setNome (e.target.value) }
+        placeholder="Digite uma tarefa:" 
+        value={input} 
+        onChange={ (e) => setInput (e.target.value) }
         /><br/>
-
-        <label>Idade: </label><br/>
-        <input 
-        placeholder="Digite sua Idade:" 
-        value={idade} 
-        onChange={ (e) => setIdade (e.target.value) }
-        /><br/>
-
-        <label>Email: </label><br/>
-        <input 
-        placeholder="Digite seu Email:" 
-        value={email} 
-        onChange={ (e) => setEmail (e.target.value) }
-        /><br/>
-
-   
       <button type="submit">Register</button>
     </form>
+
     <br/> <br/>
 
-    <div>
-      <span>Bem vindo:  {user.nome}</span><br/>
-      <span>Idade: {user.idade}</span><br/>
-      <span>Email:  {user.email}</span><br/>
-    </div>
-
-
-
+    <ul>
+    {tarefas.map( tarefa => (
+     <li key={tarefa}>{tarefa} </li>
+      ))}
+    </ul>
+  
     </div>
   );
-
 }
 
 export default App;
