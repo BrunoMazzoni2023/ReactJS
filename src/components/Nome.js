241e6fb4966e9d1f277f702796dbcca6bf5a3c91
Bruno Ferraz Mazzoni <bruno.bmxdabiciceta@gmaol.com>
2023-07-16 23:06:04 -0300

    Entendendo Props e Componentes

---
+++ src/components/Nome.js
@@ -0,0 +1,20 @@
+
+  function Nome({ aluno, idade }){
+    return(
+      <span>Bem vindo Bruno{aluno} - Idade:  {idade} anos</span>
+
+    )
+  }
+
+  export default Nome; 
+
+
+
+/*   function Nome(props){
+    return(
+      <span>Bem vindo Bruno{props.aluno} </span>
+      
+    )
+  }
+
+  export default Nome; */
